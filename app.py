7468365128c0b227bb4b7d2648d64cd0bbd8882e
ldanielcc <ldanielccastillo@gmail.com>
2024-11-13 21:07:49 -0800

    Add file upload feature

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, render_template, redirect
+import os
 
 app = Flask(__name__)
+app.config ['UPLOAD_FOLDER'] = 'uploads/'
+
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
 
 @app.route('/')
 def home():
-    return "Welcome to Secure File Upload Service"
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return 'File uploaded successfully'
 
 if __name__ == "__main__":
     app.run(debug=True)
